d1a4b2b8c2562d7e724f6b19ecbcc09a1c3139d7
Felipe De Andrade <felipe.deam98@gmail.com>
2024-02-18 03:21:04 -0300

    Ajustes para rodar localmente

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
-import Cardapio from './pages/Cardapio';
-import Inicio from './pages/Inicio';
 import Router from './routes';
 
 ReactDOM.render(
